1308846bf278fb11754d34b4c760d680d2e7d3af
Pavel1991-lab <sotnikov.pavel.91@mail.ru>
2023-11-04 01:01:59 +0300

    course_drf

--- habit/habits/serlizers.py
+++ habit/habits/serlizers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
 from habits.models import Habit
-from users.models import User
 
 
 class HabitsSerlizer(serializers.ModelSerializer):
